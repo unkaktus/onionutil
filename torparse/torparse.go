5e44ef353e10b6ce6964bd0f3d7388d710a2c0b5
Ivan Markin <twim@riseup.net>
2016-10-12 22:51:55 +0000

    go fmt on torparse.go

--- torparse/torparse.go
+++ torparse/torparse.go
@@ -8,10 +8,9 @@
 package torparse
 
 import (
-    "fmt"
-//    "log"
-    "bytes"
-    "encoding/pem"
+	"bytes"
+	"encoding/pem"
+	"fmt"
 )
 
 type TorEntry [][]byte
@@ -45,61 +44,60 @@ func (entries TorEntries) FJoined() (joined []byte) {
 var pemStart = []byte("-----BEGIN ")
 
 func ParseOutNextField(data []byte) (field string, content TorEntry, rest []byte, err error) {
-        nl_split := bytes.SplitN(data, []byte("\n"), 2)
-        if len(nl_split) != 2 {
-            return field, content, data,
-                   fmt.Errorf("Cannot split by newline")
-        }
-        /* Overwrite with the rest */
-        rest = nl_split[1]
-        sp_split := bytes.SplitN(nl_split[0], []byte(" "), -1)
-        if len(sp_split) <= 0 { /* We have no data left */
-            return field, content, data,
-                   fmt.Errorf("No data left")
-        }
+	nl_split := bytes.SplitN(data, []byte("\n"), 2)
+	if len(nl_split) != 2 {
+		return field, content, data,
+			fmt.Errorf("Cannot split by newline")
+	}
+	/* Overwrite with the rest */
+	rest = nl_split[1]
+	sp_split := bytes.SplitN(nl_split[0], []byte(" "), -1)
+	if len(sp_split) <= 0 { /* We have no data left */
+		return field, content, data,
+			fmt.Errorf("No data left")
+	}
 
 	field = string(sp_split[0])
 	content = sp_split[1:]
 	/* test if we have pem data now. if so append to previous field */
-        if bytes.HasPrefix(rest, pemStart) {
+	if bytes.HasPrefix(rest, pemStart) {
 		block, pem_rest := pem.Decode(data)
-                content = append(content, block.Bytes)
+		content = append(content, block.Bytes)
 		rest = pem_rest
 	}
-        return field, content, rest, err
+	return field, content, rest, err
 }
 
 // TODO: trim/skip empty strings/separators
 func ParseTorDocument(doc_data []byte) (docs []TorDocument, rest []byte) {
-        var doc TorDocument
-        var field string
-        var content TorEntry
-        var firstField string
+	var doc TorDocument
+	var field string
+	var content TorEntry
+	var firstField string
 
-        var parse_err error
-        for {
-            field, content, doc_data, parse_err = ParseOutNextField(doc_data)
-            //log.Printf("parsed: %v : %v", field, content)
-	    if parse_err != nil {
-                //log.Printf("Error parsing document: %v", parse_err)
-                break;
-            }
-            if firstField == "" { /* We're just in the begining - doc name */
-                firstField = field
-            }
-            if field == firstField {
-                if doc != nil {
-		    /* Append previous doc */
-                    docs = append(docs, doc)
+	var parse_err error
+	for {
+		field, content, doc_data, parse_err = ParseOutNextField(doc_data)
+		//log.Printf("parsed: %v : %v", field, content)
+		if parse_err != nil {
+			//log.Printf("Error parsing document: %v", parse_err)
+			break
 		}
-                doc = make(TorDocument)
-            }
-	    doc[field] = append(doc[field], content)
-        }
-        if doc != nil {
-            docs = append(docs, doc) /* Append a doc */
-        }
+		if firstField == "" { /* We're just in the begining - doc name */
+			firstField = field
+		}
+		if field == firstField {
+			if doc != nil {
+				/* Append previous doc */
+				docs = append(docs, doc)
+			}
+			doc = make(TorDocument)
+		}
+		doc[field] = append(doc[field], content)
+	}
+	if doc != nil {
+		docs = append(docs, doc) /* Append a doc */
+	}
 
-        return docs, doc_data
-    }
-
+	return docs, doc_data
+}
